b39a6e4ed57065bb32d3f40de890c04b4e566978
Stevan Gostojić <gostojic@uns.ac.rs>
2018-01-23 19:52:50 +0100

    Initial commit

---
+++ app/src/main/java/rs/aleph/android/example12/activities/FirstActivity.java
@@ -0,0 +1,114 @@
+package rs.aleph.android.example12.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import rs.aleph.android.example12.R;
+
+// Each activity extends Activity class
+public class FirstActivity extends Activity {
+
+	// onCreate method is a lifecycle method called when he activity is starting
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 	{
+
+		// Each lifecycle method should call the method it overrides
+		super.onCreate(savedInstanceState);
+		// setContentView method draws UI
+		setContentView(R.layout.activity_main);
+
+		// Shows a toast message (a pop-up message)
+		Toast toast = Toast.makeText(getBaseContext(), "FirstActivity.onCreate()", Toast.LENGTH_SHORT);
+		toast.show();
+	}
+
+	// onStart method is a lifecycle method called after onCreate (or after onRestart when the
+	// activity had been stopped, but is now again being displayed to the user)
+	@Override
+	protected void onStart() {
+
+		super.onStart();
+
+		// Shows a toast message (a pop-up message)
+		Toast toast = Toast.makeText(getBaseContext(), "FirstActivity.onStart()", Toast.LENGTH_SHORT);
+		toast.show();
+	}
+
+	// onRestart method is a lifecycle method called after onStop when the current activity is
+	// being re-displayed to the user
+	@Override
+    protected void onRestart() {
+
+		super.onRestart();
+
+		// Shows a toast message (a pop-up message)
+		Toast toast = Toast.makeText(getBaseContext(), "FirstActivity.onRestart()", Toast.LENGTH_SHORT);
+		toast.show();
+    }
+
+	// onResume method is a lifecycle method called after onRestoreInstanceState, onRestart, or
+	// onPause, for your activity to start interacting with the user
+	@Override
+	protected void onResume() {
+
+		super.onResume();
+
+		// Shows a toast message (a pop-up message)
+		Toast toast = Toast.makeText(getBaseContext(), "FirstActivity.onResume()", Toast.LENGTH_SHORT);
+		toast.show();
+	}
+
+	// onPause method is a lifecycle method called when an activity is going into the background,
+	// but has not (yet) been killed
+	@Override
+	protected void onPause() {
+
+		super.onPause();
+
+		// Shows a toast message (a pop-up message)
+		Toast toast = Toast.makeText(getBaseContext(), "FirstActivity.onPause()", Toast.LENGTH_SHORT);
+		toast.show();
+	}
+
+	// onStop method is a lifecycle method called when the activity are no longer visible to the user
+	@Override
+	protected void onStop() {
+
+		super.onStop();
+
+		// Shows a toast message (a pop-up message)
+		Toast toast = Toast.makeText(getBaseContext(), "FirstActivity.onStop()", Toast.LENGTH_SHORT);
+		toast.show();
+	}
+
+	// onDestroy method is a lifecycle method that perform any final cleanup before an activity is destroyed
+	@Override
+	protected void onDestroy() {
+
+		super.onDestroy();
+
+		// Shows a toast message (a pop-up message)
+		Toast toast = Toast.makeText(getBaseContext(), "FirstActivity.onDestroy()", Toast.LENGTH_SHORT);
+		toast.show();
+	}
+
+	// Called when btnStart button is clicked
+	public void btnStartActivityClicked(View view) {
+		// This is an explicit intent (class property is specified)
+        Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+		// startActivity method starts an activity
+        startActivity(intent);
+	}
+
+	// Called when btnOpen is clicked
+    public void btnOpenBrowserClicked(View view) {
+		// This is an implicit intent
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://developer.android.com"));
+		// startActivity method starts an activity
+		startActivity(i);
+    }
+}
